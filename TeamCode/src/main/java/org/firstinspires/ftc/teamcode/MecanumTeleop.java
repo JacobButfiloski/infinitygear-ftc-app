6de0573ad5b6823a97c651163c14029d4aa94108
Jacob Butfiloski <jacobbutfiloski@gmail.com>
2019-09-30 11:52:11 -0400

    Minor adjustments
    Cleaned up code. Removed import statements that are no longer used and removed old commented out code.
    
    Changelog:
    - Commented out ToggleServo() [Line 45]

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleop.java
@@ -8,39 +8,30 @@
  *
  * DONE: LIST
  * ///TODO: remove references to subassemblies that are no longer used
- *
- *
+ * ///TODO: remove legacy imports
+ * ///TODO: removed legacy functions
+ * ///TODO: removed legacy comments
  */
 
 
 
-
-
 package org.firstinspires.ftc.teamcode;
-
-import android.graphics.Color;
-
-import com.google.gson.annotations.SerializedName;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Mecanum Teleop", group="Iterative Opmode")
-public class MecanumTeleop extends OpMode
-{
+public class MecanumTeleop extends OpMode {
     private DcMotor leftMotorFront = null;
     private DcMotor rightMotorFront = null;
     private DcMotor leftMotorBack = null;
     private DcMotor rightMotorBack = null;
 
     @Override
-    public void init()
-    {
+    public void init() {
         //Motor Initialization
         leftMotorFront = hardwareMap.get(DcMotor.class, "left_motor_front");
         rightMotorFront = hardwareMap.get(DcMotor.class, "right_motor_front");
@@ -48,29 +39,27 @@ public class MecanumTeleop extends OpMode
         rightMotorBack = hardwareMap.get(DcMotor.class, "right_motor_back");
     }
 
-    public void ToggleServo(Servo servo)
-    {
+
+    //!
+    ///Do not delete, could have use at a later time
+    /*public void ToggleServo(Servo servo) {
         final double min = 0.0;
         final double max = 1.0;
 
-        if(servo.getPosition() == min)
-        {
+        if (servo.getPosition() == min) {
             servo.setPosition(max);
-        } else if(servo.getPosition() == max)
-        {
+        } else if (servo.getPosition() == max) {
             servo.setPosition(min);
         } else {
             //Failsafe
             telemetry.addData("Servo Error", "Position is not min or max, setting to min.");
             servo.setPosition(min);
         }
-    }
+    }*/
 
     @Override
-    public void loop()
-    {
+    public void loop() {
         //Direction Setting
-        //rightMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightMotorBack.setDirection(DcMotorSimple.Direction.REVERSE);
         leftMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
