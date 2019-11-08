024989149583058c1ef88f710c59cce2ca490c5d
Jacob Butfiloski <jacobbutfiloski@gmail.com>
2019-11-08 11:29:13 -0500

    Various Teleop Changes
    Made multiple changes to main teleop.
    
    CHANGELOG:
    - Added ability to control intake and outtake
    - Added ability to control the collection servo
    - Added ability to control the beacon servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INFINITYFIRSTteleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INFINITYFIRSTteleop.java
@@ -29,7 +29,11 @@ public class INFINITYFIRSTteleop extends OpMode {
     private DcMotor rightMotorFront = null;
     private DcMotor leftMotorBack = null;
     private DcMotor rightMotorBack = null;
+    private DcMotor leftIntake = null;
+    private DcMotor rightIntake = null;
 
+    private Servo armServo = null;
+    private Servo beaconServo = null;
     @Override
     public void init() {
         //Motor Initialization
@@ -38,6 +42,11 @@ public class INFINITYFIRSTteleop extends OpMode {
         rightMotorFront = hardwareMap.get(DcMotor.class, "rightMotorFront");
         leftMotorBack = hardwareMap.get(DcMotor.class, "leftMotorBack");
         rightMotorBack = hardwareMap.get(DcMotor.class, "rightMotorBack");
+
+        leftIntake = hardwareMap.get(DcMotor.class, "leftMotorIntake");
+        rightIntake = hardwareMap.get(DcMotor.class, "rightMotorIntake");
+        armServo = hardwareMap.get(Servo.class, "armServo");
+        beaconServo = hardwareMap.get(Servo.class, "beacon");
     }
 
 
@@ -61,10 +70,11 @@ public class INFINITYFIRSTteleop extends OpMode {
     @Override
     public void loop() {
         //Direction Setting
-        rightMotorFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightMotorBack.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftMotorBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightMotorBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotorFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftMotorBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightIntake.setDirection(DcMotorSimple.Direction.REVERSE);
 
         //Value Definition
         float LFspeed = gamepad1.left_stick_y - gamepad1.left_stick_x;
@@ -78,6 +88,35 @@ public class INFINITYFIRSTteleop extends OpMode {
         RFspeed = Range.clip(RFspeed, -1, 1);
         RBspeed = Range.clip(RBspeed, -1, 1);
 
+
+
+
+        if(gamepad2.right_bumper)
+        {
+            leftIntake.setPower(1);
+            rightIntake.setPower(1);
+        } else if (gamepad2.left_bumper)
+        {
+            leftIntake.setPower(-1);
+            rightIntake.setPower(-1);
+        } else {
+            leftIntake.setPower(0);
+            rightIntake.setPower(0);
+        }
+
+        if(gamepad2.x)
+        {
+            armServo.setPosition(1);
+        } else {
+            armServo.setPosition(0);
+        }
+
+        if(gamepad2.y)
+        {
+            beaconServo.setPosition(1);
+        } else {
+            beaconServo.setPosition(0);
+        }
         //Speed Setting
         leftMotorFront.setPower(LFspeed);
         leftMotorBack.setPower(LBspeed);
